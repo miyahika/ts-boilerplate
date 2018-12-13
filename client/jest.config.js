ea6053c5ee8297b259d67a7738fa018150bb5d26
sato taichi <ryushi@gmail.com>
2018-12-13 18:31:22 +0900

    z

--- client/jest.config.js
+++ client/jest.config.js
@@ -72,7 +72,7 @@ module.exports = {
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    "^@src/(.*)$": "<rootDir>/src/$1"
+    "^@pages/(.*)$": "<rootDir>/pages/$1"
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
@@ -110,7 +110,7 @@ module.exports = {
 
   // A list of paths to directories that Jest should use to search for files in
   roots: [
-    "<rootDir>/src",
+    "<rootDir>/pages",
     "<rootDir>/test"
   ],
 
