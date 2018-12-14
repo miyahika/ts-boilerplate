bb60b62b3bbd16d1bcb47ce8e334299435f23b5c
sato taichi <ryushi@gmail.com>
2018-12-14 18:03:21 +0900

    small fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
 import * as express from "express";
-import * as functions from "firebase-functions";
-import * as next from "next";
+import { https } from "firebase-functions";
+import next from "next";
 
 const dev = process.env.NODE_ENV !== "production";
 const server = next({ dev, conf: { distDir: ".next" } });
 const handle = server.getRequestHandler();
 
-export const application = functions.https.onRequest(
+export const application = https.onRequest(
   (req: express.Request, res: express.Response) => {
     // tslint:disable-next-line:no-console
     console.log(`File: ${req.originalUrl}`);
