85ffed398ed0a1e7c2994e1fbcd1e4f081b97a71
sato taichi <ryushi@gmail.com>
2018-12-14 18:46:45 +0900

    add express

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,10 +6,20 @@ const dev = process.env.NODE_ENV !== "production";
 const server = next({ dev, conf: { distDir: ".next" } });
 const handle = server.getRequestHandler();
 
+const app = express();
+app.disable("x-powered-by");
+app.get("/users/:uid", async (req: express.Request, res: express.Response) => {
+  const uid = req.params.uid;
+  res.status(200).send(`You requested user with UID = ${uid}`);
+});
+app.get("*", (req: express.Request, res: express.Response) => {
+  return handle(req, res);
+});
+
 export const application = https.onRequest(
   (req: express.Request, res: express.Response) => {
     // tslint:disable-next-line:no-console
     console.log(`File: ${req.originalUrl}`);
-    return server.prepare().then(() => handle(req, res));
+    server.prepare().then(() => app(req, res));
   }
 );
