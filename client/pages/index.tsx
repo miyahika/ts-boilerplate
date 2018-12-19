8ee355b32cbe8000c9403e8f4af162f9b710fc61
sato taichi <ryushi@gmail.com>
2018-12-19 18:16:40 +0900

    use React Hooks

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,34 +1,15 @@
 import * as React from "react";
 
-import Axios from "axios";
-import Link from "next/link";
-import Layout from "../components/layout";
-import { Detail, Show, Shows } from "../models/show";
+const index = () => {
+  const [count, setCount] = React.useState(0);
+  const clicker = () => setCount(count + 1);
 
-const index = (props: Shows) => (
-  <Layout>
-    <h1>Batman TV Shows</h1>
-    <ul>
-      {props.shows.map(({ show }: { show: Detail }) => (
-        <li key={show.id}>
-          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </Layout>
-);
-
-index.getInitialProps = async (): Promise<Shows> => {
-  const res = await Axios.get<Show[]>(
-    "https://api.tvmaze.com/search/shows?q=batman"
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={clicker}>Click me</button>
+    </div>
   );
-
-  // tslint:disable-next-line:no-console
-  console.log(`Show data fetched. Count: ${res.data.length}`);
-
-  return new Shows(res.data);
 };
 
 export default index;
